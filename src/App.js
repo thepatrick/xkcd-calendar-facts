f4f64defe4842a75df35aa57141e2792ee4b7e93
Patrick Quinn-Graham <make-contact@pftqg.com>
2018-01-20 18:53:04 +1100

    Local commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import generateRandomFact from './RandomFact.js';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { fact: generateRandomFact() };
+
+    this.boundNewFact = () => this.newFact();
+  }
+
+  newFact() {
+    this.setState({ fact: generateRandomFact() });
+  }
+
+  render() {
+    const { event, unusualManner, reason, wildCard, consequence } = this.state.fact; 
+    return (
+      <div className="outer-shell">
+        <div className="shell">
+          <div className="fact-container">
+            <div className="fact">
+                Did you know that <em>{event}</em>{' '}
+                <em>{unusualManner}</em> because of{' '}
+                <em>{reason}</em>? Apparently <em>{wildCard}</em>.
+                This <em>{consequence}</em>.
+            </div>
+          </div>
+        </div>
+
+        <button class="newFact" onClick={this.boundNewFact}>don't believe it?</button>
+
+        <div className="sosumi">
+          Inspired by <a href="https://xkcd.com/1930">xkcd 1930</a>.
+          JS from <a href="https://github.com/shreyasminocha/calendar-facts">@shreyasminocha</a>.
+          HTML by <a href="https://thepatrick.io/">@thepatrick</a>.
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
